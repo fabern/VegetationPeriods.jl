f78bbd0986ef0fb693fc7305bb82e7b32b6c9f07
Documenter.jl <documenter@juliadocs.github.io>
2023-06-08 03:34:44 +0000

    build based on 5b21e4b

---
+++ previews/PR4/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Vegperiod","page":"Vegperiod","title":"Vegperiod","text":"","category":"section"},{"location":"","page":"Vegperiod","title":"Vegperiod","text":"Documentation goes here.","category":"page"}]
+}
